b283c33f842a1e0b3fa4ff06d200d82794c5184a
Alfredo Deza <alfredodeza@gmail.com>
2012-02-15 14:59:41 -0500

    make the dispatcher do some work

--- tambo/dispatcher.py
+++ tambo/dispatcher.py
@@ -2,7 +2,7 @@
 
 class Transport(object):
     """
-    This class recieves the ``sys.argv`` list and will call the dictionary 
+    This class recieves the ``sys.argv`` list and will call the dictionary
     mappings when they match the subcommands.
 
     :param arguments: Should be the *exact* list of arguments coming from ``sys.argv``
@@ -12,8 +12,12 @@ class Transport(object):
 
     def __init__(self, arguments, mapper=None):
         self.arguments = arguments
-        self.mapper = mapper
+        self.mapper = mapper or {}
 
 
     def dispatch(self):
-        pass
+        mapper_keys = self.mapper.keys()
+        for arg in self.arguments:
+            if arg in mapper_keys:
+                instance = self.mapper.get(arg)(self.arguments)
+                return instance.parse_args()
