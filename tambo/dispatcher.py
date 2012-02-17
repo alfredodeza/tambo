321e77f2e42ef28a1324afc76e5ae4e44202414c
Alfredo Deza <alfredodeza@gmail.com>
2012-02-17 15:04:00 -0500

    add subcommand helper printing

--- tambo/dispatcher.py
+++ tambo/dispatcher.py
@@ -11,10 +11,32 @@ class Transport(Parse):
     """
 
 
-    def dispatch(self, mapper=None):
-        mapper = mapper or {}
-        mapper_keys = mapper.keys()
+    def dispatch(self):
+        mapper_keys = self.mapper.keys()
         for arg in self.arguments:
             if arg in mapper_keys:
-                instance = mapper.get(arg)(self.arguments)
+                instance = self.mapper.get(arg)(self.arguments)
                 return instance.parse_args()
+
+
+    def subhelp(self):
+        """
+        This method will look at every value of every key in the mapper
+        and will output any ``class.help`` possible to return it as a
+        string that will be sent to stdout.
+        """
+        help_text = self._get_all_help_text()
+
+        if help_text:
+            return "Available subcommands:\n\n %s\n" % ''.join(help_text)
+        return ''
+
+    def _get_all_help_text(self):
+        help_text_lines = []
+        for key, value in self.mapper.items():
+            try:
+                help_text = value.help
+            except AttributeError:
+                continue
+            help_text_lines.append("    %s \t\t %s" % (key, help_text))
+        return help_text_lines
